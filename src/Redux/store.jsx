6b47de01e119aab1647dce9d76ac1eceeddc3f8c
Sreyas M Pillai <96684846+Sreyas62@users.noreply.github.com>
2023-08-09 00:39:44 +0530

    updated refresh problem

--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -1,11 +1,21 @@
-import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
-import userReducer from "./users/user.reducer";
-import thunk from "redux-thunk";
-import { movieReducer } from "./movies/movie.reducer";
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userReducer from './users/user.reducer';
+import { movieReducer } from './movies/movie.reducer';
 
-let rootReducer = combineReducers({
-    userReducer: userReducer,
-    movieReducer: movieReducer
-})
+const rootReducer = combineReducers({
+  userReducer: userReducer,
+  movieReducer: movieReducer
+});
 
-export const store = legacy_createStore(rootReducer,applyMiddleware(thunk))
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
