d3557110c7d3be10b907c107c9af3b223ceaf660
Sreyas M Pillai <96684846+Sreyas62@users.noreply.github.com>
2023-08-12 19:58:39 +0530

    updated

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -6,7 +6,7 @@ export default function PrivateRoute({ children}) {
     const {auth} = useSelector((state)=>state.userReducer)
 
     if (auth) {
-        return <MoviesPage/>
+        return children
     }
 
     return <LoginPage/>
