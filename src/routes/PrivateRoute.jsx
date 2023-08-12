764b2645a612dd88c34856828531e3af1b1a9cb5
Sreyas M Pillai <96684846+Sreyas62@users.noreply.github.com>
2023-08-12 19:43:00 +0530

    updated

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,12 @@
 import { useSelector } from "react-redux"
 import LoginPage from "../pages/LoginPage"
+import MoviesPage from "../pages/MoviesPage"
 
 export default function PrivateRoute({ children}) {
     const {auth} = useSelector((state)=>state.userReducer)
 
     if (auth) {
-        return children
+        return <MoviesPage/>
     }
 
     return <LoginPage/>
